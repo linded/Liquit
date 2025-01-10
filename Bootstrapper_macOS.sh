0c22f4712bb7ccb15d78463e8ab3744aa2a4aae6
Donny Van der Linde <93385046+linded@users.noreply.github.com>
2025-01-10 14:37:09 +0100

    Add files via upload

---
+++ Bootstrapper_macOS.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+# This script will install Application Workspace Universal Agent on macOS with the Bootstrapper.
+# Author Donny van der Linde
+# Version 1.0
+# Date created 08-01-2025
+# Copyright 2025 Recast Software
+
+# This code is provided "as is," with no guarantees or warranties of any kind
+# The user assumes all risks associated with the use and results of this code.
+
+# Set variables
+destinationFolder="/tmp/ApplicationWorkspace/"
+bootstrapper_download_url="https://download.liquit.com/extra/Bootstrapper/"
+container_url="https://liquittest.blob.core.windows.net/donny/"
+sas_token="?sp=r&st=2025-01-08T15:36:45Z&se=2027-12-31T23:36:45Z&spr=https&sv=2022-11-02&sr=c&sig=wqOcdqqZtU6znPHXj6YPduXjUDyZmadZnkf0iJSoYLM%3D"
+
+container="macOS/"
+strapper="AgentBootstrapper-Mac-2.1.0.2"
+cert="AgentRegistration.cer"
+config="Agent.json"
+
+# Check if Application Workspace is already installed
+if [ -d “/Applications/Liquit.app” ]; then
+exit 0
+fi
+
+# Create folder /tmp/Liquit/
+if [ ! -d "$destinationFolder" ]; then
+    mkdir -p "$destinationFolder"
+fi
+
+# Download the files
+curl -o "${destinationFolder}${strapper}" "${bootstrapper_download_url}${strapper}"
+curl -o "${destinationFolder}${cert}" "${container_url}${cert}${sas_token}"
+curl -o "${destinationFolder}${config}" "${container_url}${container}${config}${sas_token}"
+
+# Install with Liquit Agent BootStrapper
+sudo chmod +x "${destinationFolder}${strapper}"
+sudo xattr -dr com.apple.quarantine "${destinationFolder}${strapper}"
+cd "$destinationFolder" || exit
+sudo ./"${strapper}" --startDeployment --waitForDeployment --logPath="${destinationFolder}" --certificate="./${cert}"
+
+exit 0
